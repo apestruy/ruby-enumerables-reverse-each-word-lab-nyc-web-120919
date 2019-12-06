1f436184637afe5c26f97a298aaf9ce49c09c0d5
Alexandra Pestruyeva <apestruy@gmail.com>
2019-12-06 02:54:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,6 +7,7 @@ def reverse_each_word(sentence)
 end
 
 def reverse_each_word(sentence)
-  
-  
+  sentence_array = sentence.split(" ")
+  sentence_array.collect { |word| word.reverse! }
+  reversed_sentence = sentence_array.join(" ")
 end 
